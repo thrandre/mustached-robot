aa3fff82a45169506c0df895e6c146a397490677
Thomas Rene Andresen <thomas.r.andresen@hotmail.no>
2014-02-21 14:59:03 +0100

    Formatting in app.ts

--- PatternBonanza/app.ts
+++ PatternBonanza/app.ts
@@ -11,17 +11,18 @@ class Person {
 IoC.setup({ deferredFactory: new Deferred.DeferredFactory() });
 IoC.autoResolve(window["dependencies"]);
 
-IoC.resolve(new Locators.IIEnumerableFactory()).then(enumerableFactory => {
-    var people = [
-        new Person("Caroline", 24, "female"),
-        new Person("Thomas", 26, "male"),
-        new Person("Lasse", 21, "male")
-    ];
+IoC.resolve(new Locators.IIEnumerableFactory()).then(
+    enumerableFactory => {
+        var people = [
+            new Person("Caroline", 24, "female"),
+            new Person("Thomas", 26, "male"),
+            new Person("Lasse", 21, "male")
+        ];
 
-    enumerableFactory
-        .fromArray(people)
-        .groupBy(p=> p.age > 25)
-        .orderByAscending(g => g.sum(p => p.age))
-        .toList()
-        .each(g => console.log(g));
-});
+        enumerableFactory
+            .fromArray(people)
+            .groupBy(p=> p.age > 25)
+            .orderByAscending(g => g.sum(p => p.age))
+            .toList()
+            .each(g => console.log(g));
+    });
